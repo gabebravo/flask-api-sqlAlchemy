c8923095095fe2816f54064df90dce150d9b6ff8
Gabe Bravo <gabecbravo@gmail.com>
2020-12-07 22:31:45 -0500

    all the basic files to get the project started.

---
+++ create_tables.py
@@ -0,0 +1,15 @@
+import sqlite3
+
+connection = sqlite3.connect('data.db')  # db file
+
+cursor = connection.cursor()
+
+# users + columns // id INTEGER PRIMARY KEY = syntax for auto-incrmenting
+create_users_table = 'CREATE TABLE if NOT EXISTS users (id INTEGER PRIMARY KEY, username text, password text)'
+cursor.execute(create_users_table)
+
+create_items_table = 'CREATE TABLE if NOT EXISTS items (id INTEGER PRIMARY KEY, name text, price real)'
+cursor.execute(create_items_table)
+
+connection.commit()
+connection.close()  # tells sql to close db connection
